ea639e5f757da7c01776854442884c3f2b1668bb
Henrique <hp_bonfim@hotmail.com>
2020-09-18 20:21:12 -0300

    Security config

--- backend/src/main/java/com/projeto/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/projeto/backend/config/SecurityConfig.java
@@ -1,11 +1,10 @@
 package com.projeto.backend.config;
-
 import java.util.Arrays;
 
-import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
