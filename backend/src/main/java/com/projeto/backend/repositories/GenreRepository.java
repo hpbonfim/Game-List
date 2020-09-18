c47d2723dda3fb9240e63b81cbe131227ae1d10e
Henrique <hp_bonfim@hotmail.com>
2020-09-18 11:04:40 -0300

    DTO, Service

--- backend/src/main/java/com/projeto/backend/repositories/GenreRepository.java
+++ backend/src/main/java/com/projeto/backend/repositories/GenreRepository.java
@@ -1,9 +1,11 @@
 package com.projeto.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.projeto.backend.entities.Genre;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long>{
 	
 
