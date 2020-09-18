62d08fb06cca8addcce2ccf62b178236fb818865
Henrique <hp_bonfim@hotmail.com>
2020-09-18 16:21:54 -0300

    POST records

--- backend/src/main/java/com/projeto/backend/repositories/RecordRepository.java
+++ backend/src/main/java/com/projeto/backend/repositories/RecordRepository.java
@@ -3,10 +3,14 @@ package com.projeto.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.projeto.backend.entities.Genre;
+import com.projeto.backend.entities.Record;
 
 @Repository
-public interface RecordRepository extends JpaRepository<Genre, Long>{
+public interface RecordRepository extends JpaRepository<Record, Long>{
 	
-
+/*	@Query("SELECT obj FROM Record obj WHERE " 
+			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
+*/
 }
